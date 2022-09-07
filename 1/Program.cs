0c3b8c3850b6639576349b4f9bd99185c8c49a91
Yuriy <khvan-yura@mail.ru>
2022-09-07 16:59:46 +0300

    Commit with pic

--- 1/Program.cs
+++ 1/Program.cs
@@ -4,14 +4,12 @@ string[] array1 = new string[5] {"012", "hello", "45", "dim", "piece"};
 string[] array2 = new string[array1.Length];
 void LastArrayRes(string[] array1, string[] array2)
 {
-    int count = 0;
     for (int i = 0; i < array1.Length; i++)
     {
     if(array1[i].Length <= 3)
         {
-        array2[count] = array1[i];
-        count++;
-        }
+        array2[i] = array1[i];
+                }
     }
 }
 void PrintArray(string[] array)
